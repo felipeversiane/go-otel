9f30922c8f08df46d8dbe0401212a8eeefc42069
felipeversiane <felipeversiane09@gmail.com>
2024-10-19 15:08:02 -0300

    feat: adding a server service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/felipeversiane/go-otel/internal/infra/config"
+	"github.com/felipeversiane/go-otel/internal/infra/config/log"
+	"github.com/felipeversiane/go-otel/internal/infra/server"
+	"github.com/felipeversiane/go-otel/internal/infra/services/database"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	cfg := config.NewConfig()
+
+	logger := log.NewLogger(cfg.GetLogConfig())
+	logger.Configure()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	database := database.NewDatabaseConnection(ctx, cfg.GetDatabaseConfig())
+	defer database.Close()
+
+	server := server.NewServer(cfg.GetServerConfig(), database)
+	server.SetupRouter()
+	server.Start()
 }
